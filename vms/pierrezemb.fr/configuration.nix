ebec758352bd50444da9611e81a858b14b144e4c
Pierre Zemb <contact@pierrezemb.fr>
2022-10-30 14:55:52 +0100

    vm: mapAttrs

--- vms/pierrezemb.fr/configuration.nix
+++ vms/pierrezemb.fr/configuration.nix
@@ -1,9 +1,10 @@
 { lib, config, pkgs, ... }:
 
 let
-  websites = [
-    { name = "website"; localPath = "/var/www/pierrezemb"; remotePath = "https://github.com/PierreZ/portfolio.git"; }
-  ];
+  websites = {
+    portfolio = {localPath = "/var/www/pierrezemb"; remotePath = "https://github.com/PierreZ/portfolio.git"; };
+  };
+
 in
 {
   environment.systemPackages = with pkgs; [
@@ -23,24 +24,24 @@ in
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC/UZhx9DiyKdsOvF5QwKJnq+th8lTurJVwPZZ2VyNGZwOgYorul2RNZNDeD9IzOT25TezUcbkZEFRqsuxjpEvx9dpyrhqj/waughSqeJaRcLmqs3+JqDCricyVIs0upiGSPMsSUMy/Rzxb5TCj1ZHope8WTwxkaAlNd7kZ2grJIduMnz9e4kgP11HqCTGopfeBplTcn4ovs61OiN57HiYIsvh5vXahu4HY6SsvAZ92i3FvghO0EwR7I8DXuMgyzA2mXnidzxyMJ2Bu1JPytCJeCC3O+wtWFCOgh8LmJ1eufn52OxbANsXw9jlWJwpx3zecEcUCfHb4pZrEYa4hlh2/reSYv9W1NFE46O5KfqmoGtbUYFJCKUJH83Ju05c0uWS+SU7wr/xKdwWvXhjt2ZgWIJ852k0rle8Is3WA+WV5Rx24xX/FMXpGmISLt3C2FUCUGvNnsUeVF0KvRibyi/AGiyI7up6te7NrEcL/EUd6c2wFUk+31TCtqcICf9JJEQboedgwv83rgLPBrKMfxcMPE4vvPZPV6sATelPBsgZ8p5hRqc95HGwIsS2LFppEq9Z5eDJOiXi9FgQlIow9XBWnmlZQRja8nvkuBVpRiA4Mv31VWgV4hzIOYx72WNGmTQBMY4SR9hpE2SVHD2YznXY38o6V3kpMPOwVVCeoToV6jw== contact@pierrezemb.fr" # content of authorized_keys file
   ];
 
-  systemd = lib.forEach websites (w: {
+  systemd = lib.mapAttrs (name: w: {
     # Setup timer and unit to pull every minute the websites 
-    services."pull-${w.name}" = {
+    services."pull-${name}" = {
       serviceConfig.Type = "oneshot";
       path = with pkgs; [ git ];
       script = ''
         cd ${w.localPath} && git pull origin master
       '';
     };
-    timers."pull-${w.name}" = {
+    timers."pull-${name}" = {
       wantedBy = [ "timers.target" ];
-      partOf = [ "pull-${w.name}.service" ];
+      partOf = [ "pull-${name}.service" ];
       timerConfig = {
         OnCalendar = "minutely";
-        Unit = "pull-${w.name}.service";
+        Unit = "pull-${name}.service";
       };
     };
-  });
+  }) websites;
 
   virtualisation.oci-containers.containers.hew = {
     autoStart = true;
